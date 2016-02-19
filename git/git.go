516e1f220a7b14fd748a792b0c28e57db634e5bd
Adam Hintz <adamh.zero@gmail.com>
2016-02-18 22:30:25 -0800

    git.go: fix bug with error formatting

--- git/git.go
+++ git/git.go
@@ -12,8 +12,8 @@ import (
 func DetectRoot() (string, error) {
 	output, err := shell.Run("git", "rev-parse", "--show-toplevel")
 	if err != nil {
-		return "", fmt.Errorf("Error: %s\n(Hint: this error may happen if you run `sync` without " +
-			"any params and outside of any git repository)\n")
+		return "", fmt.Errorf("Error: %v\n(Hint: this error may happen if you run `sync` without " +
+			"any params and outside of any git repository)\n", err)
 	}
 
 	return strings.Trim(filepath.Base(output), "\n "), nil
