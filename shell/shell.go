8be4cae7bb1a55276fa65259ed382084cccbbceb
Adam Hintz <adamh.zero@gmail.com>
2016-02-18 22:31:22 -0800

    shell: add unix-specific Tempdir

--- shell/shell.go
+++ shell/shell.go
@@ -1,9 +1,13 @@
+// +build !windows
 package shell
 
 import (
 	"os"
 	"os/exec"
+	"strings"
 )
+
+var tmpDir = ""
 
 // Run is a no muss, no fuss way to run a command in a shell.
 func Run(command ...string) (string, error) {
@@ -11,4 +15,36 @@ func Run(command ...string) (string, error) {
 	cmd.Stderr = os.Stderr
 	output, err := cmd.Output()
 	return string(output), err
+}
+
+// TempDir makes and returns a temporary directory. Any provided arguments will
+// be evaluated as successive directories within that temporary directory.
+// Unix-only.
+func TempDir(subdir ...string) (string, error) {
+	if tmpDir == "" {
+		// Try to mktmp the base directory
+		dirPrefix := "temp"
+		if len(subdir) > 0 {
+			dirPrefix = subdir[0]
+		}
+
+		dir, err := Run("mktemp", "-d", "-t", dirPrefix)
+		if err != nil {
+			return "", err
+		}
+		tmpDir = strings.Trim(dir, "\n\t ")
+	}
+
+	// No subdirectory
+	if len(subdir) < 2 {
+		return tmpDir, nil
+	}
+
+	// Make the directories underneath the base directory
+	fullPath := tmpDir + strings.Join(subdir[1:], "/")
+	if err := os.MkdirAll(fullPath, os.ModeDir); err != nil {
+		return "", err
+	}
+
+	return fullPath, nil
 }
