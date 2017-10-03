e6a2ef087f7d3ce964542bb38ce2cd1888d3588c
Adam Hintz <adamh.zero@gmail.com>
2017-10-02 19:11:40 -0700

    Add Deferrer package for deferring a whole bunch of things at once

---
+++ deferrer/deferrer.go
@@ -0,0 +1,28 @@
+package deferrer
+
+// Deferrer allows for collecting many defer statements for deferring later on.
+type Deferrer struct {
+	funcs []func()
+}
+
+// Cleanup calls the statements that were added to the Deferrer's stack, in reverse order (the defer
+// statement works like a stack: https://tour.golang.org/flowcontrol/13)
+func (d *Deferrer) Cleanup() {
+	for i := len(d.funcs) - 1; i >= 0; i-- {
+		d.funcs[i]()
+	}
+
+	d.funcs = []func(){}
+}
+
+// Defer adds a function to the Deferrer's stack.
+func (d *Deferrer) Defer(f func()) {
+	d.funcs = append(d.funcs, f)
+}
+
+// New creates a new Deferrer for defering actions on *your* timetable.
+func New() Deferrer {
+	return Deferrer{
+		funcs: make([]func(), 0),
+	}
+}
