41a5b438232b2e84b269b19a25e6b5f070d1afc3
Adam Hintz <adamh.zero@gmail.com>
2020-12-12 13:41:37 -0800

    Add the reusable parts of 'check-git-dirs' to shell, git

--- git/git.go
+++ git/git.go
@@ -9,12 +9,34 @@ import (
 )
 
 // Try to get the root directory of this repo
+// Deprecated: use IsGitRoot() instead
 func DetectRoot() (string, error) {
 	output, err := shell.Run("git", "rev-parse", "--show-toplevel")
 	if err != nil {
-		return "", fmt.Errorf("Error: %v\n(Hint: this error may happen if you run `sync` without " +
+		return "", fmt.Errorf("Error: %v\n(Hint: this error may happen if you run `sync` without "+
 			"any params and outside of any git repository)\n", err)
 	}
 
 	return strings.Trim(filepath.Base(output), "\n "), nil
+}
+
+// A predicate to determine if `dir` is the root of a git repository.
+// Combos well with `shell.ParseDirectory`, for example.
+func IsGitRoot(dir string) (bool, error) {
+	// Try `git rev-parse --show-toplevel` at our current directory
+	result, stderrText, err := shell.RunInDir(dir, "git", "rev-parse", "--show-toplevel")
+	if err != nil {
+		if !strings.Contains(stderrText, "not a git repository") {
+			return false, fmt.Errorf("Failed to run git rev-parse: %w", err)
+		}
+		return false, nil
+	}
+
+	// Make sure we're in the base -- if we ever hit this error, we have... big problems
+	// Not 100% sure this is an exactly accurate way to find this out, might need os.SameFile
+	if strings.TrimSpace(result) != dir {
+		return false, fmt.Errorf("Job directory '%s' is not the base git directory '%s'", dir, result)
+	}
+
+	return true, nil
 }
--- shell/shell.go
+++ shell/shell.go
@@ -1,18 +1,100 @@
 package shell
 
 import (
+	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"time"
 )
 
 // Run is a no muss, no fuss way to run a command in a shell.
+// Deprecated: use RunInDir() instead
 func Run(command ...string) (string, error) {
 	cmd := exec.Command(command[0], command[1:]...)
 	cmd.Stderr = os.Stderr
 	output, err := cmd.Output()
 	return string(output), err
+}
+
+// A DirMatch is a predicate to determine whether a given directory (as a string)
+// matches a set of rules
+type DirMatch func(dir string) (bool, error)
+
+// ParseDirectory checks if `dir` fits a predicate `matches`. If it encounters an error it
+// cannot proceed from, it returns `stopErr`. Otherwise, it returns all `subdirs` for the
+// parent to recurse into.
+func ParseDirectory(matches DirMatch, dir string) (isRoot bool, subdirs []string, stopErr error) {
+	if stat, err := os.Stat(dir); err != nil {
+		stopErr = fmt.Errorf("Could not read root directory: %w", err)
+		return
+	} else if !stat.IsDir() {
+		stopErr = fmt.Errorf("'%s' is not a directory", dir)
+		return
+	}
+
+	isMatch, err := matches(dir)
+	if err != nil {
+		stopErr = fmt.Errorf("Running command in directory '%s': %w", dir, err)
+		return
+	} else if !isMatch {
+		// Recurse within
+		files, dirErr := ioutil.ReadDir(dir)
+		if dirErr != nil {
+			stopErr = fmt.Errorf("Reading directory '%s': %w", dir, dirErr)
+			return
+		}
+
+		subdirs := make([]string, 0)
+		for _, fileInfo := range files {
+			if fileInfo.IsDir() {
+				nextDir := filepath.Join(dir, fileInfo.Name())
+				subdirs = append(subdirs, nextDir)
+			}
+		}
+		return false, subdirs, nil
+	}
+
+	return true, nil, nil
+}
+
+// RunInDir runs `command` from `directory`, and returns any text from the `command` or `err` that occurred.
+func RunInDir(directory string, command ...string) (stdoutText string, stderrText string, err error) {
+	cmd := exec.Command(command[0], command[1:]...)
+	cmd.Dir = directory
+
+	// Create Pipes
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", "", fmt.Errorf("Failed to create stdout pipe: %w", err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return "", "", fmt.Errorf("Failed to create stderr pipe: %w", err)
+	}
+
+	// Start Command
+	if err = cmd.Start(); err != nil {
+		return "", "", fmt.Errorf("Failed to start command: %w", err)
+	}
+
+	// Read From Pipes
+	stdoutBytes, stdoutErr := ioutil.ReadAll(stdout)
+	if stdoutErr != nil {
+		return "", "", fmt.Errorf("Failed reading stdout (%w) (perhaps another error: %v)", stdoutErr, err)
+	}
+	outputStr := string(stdoutBytes)
+
+	stderrBytes, stderrErr := ioutil.ReadAll(stderr)
+	if stderrErr != nil {
+		return outputStr, "", fmt.Errorf("Failed reading stderr (%w) while trying to deal with command failure: %v", stderrErr, err)
+	}
+
+	// Finish Command
+	err = cmd.Wait()
+	return outputStr, string(stderrBytes), err
 }
 
 // TempDir gets the system temporary directory and makes a random directory
