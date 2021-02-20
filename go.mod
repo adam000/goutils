c7187bae216dbcf7051a0e67f4f6312b175df3f1
Adam Hintz <adamh.zero@gmail.com>
2021-02-19 20:23:02 -0800

    healthcheck: add ridicously simple ping app

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/mux v1.8.0
 	go4.org v0.0.0-20200104003542-c7e774b10ea0
 	golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d
 	google.golang.org/appengine v1.6.5
---
+++ healthcheck/ping/main.go
@@ -0,0 +1,33 @@
+// Ping provides a stupid simple healthchecker, with an optional argument for port.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/adam000/goutils/healthcheck"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	port := ":8080"
+	if len(os.Args) != 0 {
+		i, err := strconv.Atoi(os.Args[0])
+		if err != nil {
+			log.Fatalf("Could not convert %s to string:%v", os.Args[0], err)
+		}
+		if i <= 1024 || i > 65535 {
+			log.Fatalf("Port %d outside of valid range (1024, 65535]", i)
+		}
+		port = fmt.Sprintf(":%d", i)
+	}
+	r := mux.NewRouter()
+
+	r.HandleFunc("/health", healthcheck.PingHealthcheckHandler)
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
